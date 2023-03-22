b4a2745d0e8399cfe4d8d54bbf90b088d72023dc
Nick Marshall <marshalllele@uri.edu>
2023-03-22 13:15:05 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,28 +1,101 @@
 var http = require('http');
 var fs = require('fs');
-const PORT = 1337;
+var port = 1337;
 
-let serveStaticFile = function(filePath, responseCode) {
+function serveStaticFile(response, path, contentType, responseCode) {
     if (!responseCode) {
-        responseCode =200;
+        responseCode = 200;
     }
-    fs. readFile(_dirname + path, function(err,data) {
+
+    fs.readFile(__dirname + path, function(err, data) {
         if (err) {
-            res. writeHead(500, {'Content—Type': 'text/plain'});
-            res.end( '500 — Internal Error');
+            response.writeHead (500, { 'Content-Type': 'text/plain'});
+
+            response.end('500 - internal error');
         } else {
-            res.writeHead(responseCode,
-                { 'Content—Type': contentType });
-            res.end(data);
+            response.writeHead(responseCode, {
+                'Content-Type': contentType
+            });
+
+            response.end(data);
         }
     });
 }
 
-http.createServer((request, response) => {
+http.createServer(function(request, response) {
+    var path = request.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    response.write("Hello World!\n");
-    response.end();
-}).listen(PORT);
+    switch(path) {
+
+        case '': 
+            serveStaticFile(response, '/public/index.html', 'text/html');
+            break;
+        
+        case '/index':
+            serveStaticFile(response, '/public/index.html', 'text/html');
+            break;
+        
+        case '/about':
+            serveStaticFile(response, '/public/about.html', 'text/html');
+            break;
+
+        case '/contact':
+            serveStaticFile(response, '/public/contact.html', 'text/html');
+            break;
+
+        case '/posts':
+            serveStaticFile(response, '/public/posts.html', 'text/html');
+            break;
+
+        case '/under-construction':
+            serveStaticFile(response, '/public/under-construction.html', 'text/html');
+            break;
+
+        case '/css/style.css':
+            serveStaticFile(response, '/public/css/style.css', 'text/css');
+            break;
+
+        case '/images/404bottom.gif':
+            serveStaticFile(response, '/public/images/404bottom.gif', 'image/gif');
+            break;
+
+        case '/images/404mid.gif':
+            serveStaticFile(response, '/public/images/404mid.gif', 'image/gif');
+            break;
+
+        case '/images/404top_w.jpg':
+            serveStaticFile(response, '/public/images/404top_w.jpg', 'image/jpeg');
+            break;
+
+        case '/images/blogging.png':
+            serveStaticFile(response, '/public/images/blogging.png', 'image/png');
+            break;
+
+        case '/images/computer-typing.jpeg':
+            serveStaticFile(response, '/public/images/computer-typing.jpeg', 'image/jpeg');
+            break;
+
+        case '/images/construction.png':
+            serveStaticFile(response, '/public/images/construction.png', 'image/png');
+            break;
+
+        case '/images/logo.png':
+            serveStaticFile(response, '/public/images/logo.png', 'image/png');
+            break;
+
+        case '/images/merch.png':
+            serveStaticFile(response, '/public/images/merch.png', 'image/png');
+            break;
+
+        case '/images/x.png':
+            serveStaticFile(response, '/public/images/x.png', 'image/png');
+            break;
+
+        default:
+            serveStaticFile(response, '/public/404.html', 'text/html');
+            break;
+    }
+
+}).listen(port);
+
+console.log('Listening... go to http://localhost:' + port);
